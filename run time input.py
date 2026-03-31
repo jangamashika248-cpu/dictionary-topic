7a6b6c80e1545a5f8b2954c7ef5aa7277b61d855
jangamashika248-cpu <jangamashika248@gmail.com>
2026-03-31 20:36:47 +0530

    today i completed run time input topic

---
+++ run time input.py
@@ -0,0 +1,73 @@
+#run_time input()
+'''a=3
+b=6
+print(a+b)'''
+
+
+'''a=3
+b=8
+c=a+b
+print(c)'''
+
+#print(4+7)
+
+'''a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+
+
+'''a=str(input("enter the a value"))
+b=str(input("enter the b value"))
+print(a+b)'''
+
+
+'''a=input("enter the a value")
+b=input("enter the b value")
+print(a+b)'''
+
+'''a=complex(input("enter the a value"))
+b=complex(input("enter the b value"))
+print(a+b)'''
+
+'''a=float(input("enter the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+
+
+'''fname=input("firstname")
+lname=input("lastname")
+print((fname+lname).title())'''
+
+'''a=bool(input("enter the a value"))
+b=bool(input("enter the b value"))
+c=bool(input("enter the c value"))
+print(a+b+c)'''
+
+
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+'''a=int(input("a value"))
+b=int(input("a value"))
+option=input("choose the option add sub mul")
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+
+'''a=int(input())
+b=int(input())
+print(a+b)'''
+
+'''a=input()
+print(a)'''
+
+
+
+
